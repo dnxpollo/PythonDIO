84a9a03f569b03a8c9544fbeb831e739536f4cf1
Sérgio Henrique <dnxpollo@gmail.com>
2025-11-16 21:25:24 -0300

    Retirada de arquivo esboço e alteração do nome do arquivo do desafio.

--- desafio/desafio.py
+++ desafio/desafio.py
@@ -1,55 +1,87 @@
 menu = '''
 
-    [d] Depositar
-    [s] Sacar
-    [e] Extrato
-    [q] Sair
-    '''
+[d] Depositar
+[s] Sacar
+[e] Extrato
+[q] Sair
 
+'''
 
+# Variáveis principais
 saldo = 0
 limite = 500
 extrato = ''
 numero_saques = 0
 LIMITE_SAQUES = 3
 
-while True:
+# Função depositar
+def depositar(saldo, extrato):
+    print('        Depósito        ')
+    print('-------------------------')
+    valor_deposito = int(input('Deseja fazer um depósito em qual valor: '))
 
+    if valor_deposito <= 0:
+        print('Valor precisa ser positivo e maior que zero!')
+        return saldo, extrato  # ❗ Corrigido: não chama a função de novo
+
+    saldo += valor_deposito
+    extrato += f'Depósito: R$ {valor_deposito:.2f}\n'
+    print(f'Depósito de R$ {valor_deposito:.2f} realizado com sucesso!')
+    return saldo, extrato
+
+# Função sacar
+def sacar(saldo, extrato, numero_saques):
+    print('|      Saque        |')
+    print('---------------------')
+    valor_saque = int(input('Deseja realizar um saque em qual valor?  '))
+
+    if valor_saque <= 0:
+        print('O valor precisa ser maior que zero.')
+        return saldo, extrato, numero_saques
+
+    if valor_saque > limite:
+        print('Valor de saque excedido! Limite é R$500.00.')
+        return saldo, extrato, numero_saques
+
+    if valor_saque > saldo:
+        print('Saldo insuficiente.')
+        return saldo, extrato, numero_saques
+
+    if numero_saques >= LIMITE_SAQUES:
+        print('Limite diário de saques atingido.')
+        return saldo, extrato, numero_saques
+
+    saldo -= valor_saque
+    extrato += f'Saque: R$ {valor_saque:.2f}\n'
+    numero_saques += 1
+    print(f'Saque de R$ {valor_saque:.2f} realizado com sucesso.')
+    return saldo, extrato, numero_saques
+
+# Função extrato
+def exibir_extrato(saldo, extrato):
+    print('\n========== EXTRATO ==========')
+    print(extrato if extrato else 'Nenhuma movimentação realizada.')
+    print(f'Saldo atual: R$ {saldo:.2f}')
+    print('==============================\n')
+
+# Loop principal
+while True:
     opcao = input(menu)
 
-
     if opcao == 'd':
-        deposito = int(input('Deseja fazer um deposito em qual valor: '))
-        print('Deposito')
-        ('--------------')
-        print(f'O seu deposito foi feito no valor de R${deposito}. Deseja voltar ao menu ? [1] Sim [2] Encerrar programa')
-        deposito =+ saldo
-        opcao_deposito = int(input('Qual opção devemos seguir? '))
-        if opcao_deposito == 1:
-            continue
-        else:
-            break
+        saldo, extrato = depositar(saldo, extrato)
+
     elif opcao == 's':
-        print('Saque')
-        print("---------")
-        valor_saque = int(input('Deseja realizar um saque em qual valor?  '))
-        if valor_saque < saldo:
-            print('Saldo insulficiente. Caso necessário, entre em contato com seu gerente.')
-            continue
-        elif valor_saque <= 0:
-            print('O valor precisa ser maior que zero.')
-            continue
-        elif valor_saque > 500:
-            print('O valor limite para saques é R$500.0!')
-        elif valor_saque > saldo:
-            numero_saques = +1
-            saldo = valor_saque - saldo 
+        saldo, extrato, numero_saques = sacar(saldo, extrato, numero_saques)
 
     elif opcao == 'e':
-        print('Extrato')
+        exibir_extrato(saldo, extrato)
+
     elif opcao == 'q':
-        print('Sair')
+        print('Saindo do sistema...')
         break
+
     else:
-        print('Opção inválida. Escolha uma opção do menu')
+        print('Opção inválida. Tente novamente.')
+
 
